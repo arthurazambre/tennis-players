21d425624a092e74e38e3c0ddcfabf2f83bbd921
arthurazambre <arthur@azambre.net>
2023-09-14 17:13:38 +0200

    Spring Boot Application

---
+++ src/test/java/com/example/tennisplayers/TennisPlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tennisplayers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisPlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
