c048e1c37ccfa699b5b995021c05a8dfebb8e6ba
arthurazambre <arthur@azambre.net>
2023-09-14 19:38:10 +0200

    Player unfound 404

--- src/main/java/com/example/tennisplayers/controller/TennisPlayersController.java
+++ src/main/java/com/example/tennisplayers/controller/TennisPlayersController.java
@@ -4,6 +4,8 @@ import com.example.tennisplayers.model.TennisPlayerData;
 import com.example.tennisplayers.model.TennisPlayersData;
 import com.example.tennisplayers.service.TennisPlayersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +21,18 @@ public class TennisPlayersController {
     }
 
     @GetMapping("/players")
-    public TennisPlayersData playersGet() {
-        return tennisPlayersService.getData();
+    public ResponseEntity<TennisPlayersData> playersGet() {
+        return ResponseEntity.ok(tennisPlayersService.getData());
     }
 
     @GetMapping("/players/{id}")
-    public TennisPlayerData playersGetById(@PathVariable Long id) {
-        return tennisPlayersService.getPlayerData(id);
+    public ResponseEntity<TennisPlayerData> playersGetById(@PathVariable Long id) {
+        TennisPlayerData result = tennisPlayersService.getPlayerData(id);
+
+        if (result == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.ok(tennisPlayersService.getPlayerData(id));
     }
 }
